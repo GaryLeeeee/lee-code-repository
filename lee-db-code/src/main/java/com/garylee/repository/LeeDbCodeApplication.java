57ba35f78e78d4cfc99a818faf594499c76ff501
GaryLeeeee <664306561@qq.com>
2021-07-20 21:05:42 +0800

    创建子module

---
+++ lee-db-code/src/main/java/com/garylee/repository/LeeDbCodeApplication.java
@@ -0,0 +1,13 @@
+package com.garylee.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeeDbCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeeDbCodeApplication.class, args);
+	}
+
+}
