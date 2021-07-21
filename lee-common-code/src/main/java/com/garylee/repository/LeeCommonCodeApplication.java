d6afe50929faddad015631564dd550e26c633d3f
GaryLeeeee <664306561@qq.com>
2021-07-21 22:36:29 +0800

    新建通用代码库

---
+++ lee-common-code/src/main/java/com/garylee/repository/LeeCommonCodeApplication.java
@@ -0,0 +1,13 @@
+package com.garylee.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeeCommonCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeeCommonCodeApplication.class, args);
+    }
+
+}
